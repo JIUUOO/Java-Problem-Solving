8e17e6689c0b52c5154fe0e5f0877eff8b320d45
JIUUOO <nninjiuuoo@gmail.com>
2024-05-07 22:00:36 +0900

    feat: add p2560

---
+++ src/baekjoon/gold3/P2460.java
@@ -0,0 +1,50 @@
+package baekjoon.gold3;
+
+import java.util.Scanner;
+
+public class P2460 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        Train2 train = new Train2();
+
+        for (int i = 0; i < 10; i++) {
+            train.getOut(keyboard.nextInt());
+            train.getOn(keyboard.nextInt());
+        }
+
+        System.out.println(train.getMaxNum());
+    }
+}
+
+class Train2 {
+    private int num;
+    private int maxNum;
+
+    public Train2() {
+        num = 0;
+        maxNum = -1; // initialize to -1 to initialize for once
+    }
+
+    public void getOn(int num) {
+        this.num += num;
+        this.calcMaxNum();
+    }
+
+    public void getOut(int num) {
+        this.num -= num;
+        this.calcMaxNum();
+    }
+
+    public void calcMaxNum() {
+        if (maxNum > -1 && num > maxNum) {
+            maxNum = num;
+        } else if (maxNum == -1) {
+            maxNum = num;
+        }
+    }
+
+    public int getMaxNum() {
+        return this.maxNum;
+    }
+}
